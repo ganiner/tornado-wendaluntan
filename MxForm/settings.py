c3be97dc080e52ecd78737fb4466d5baac513759
agamgn <agamgn@163.com>
2019-06-11 21:13:09 +0800

    add setting

--- MxForm/settings.py
+++ MxForm/settings.py
@@ -2,13 +2,18 @@
 #@Auhor : Agam
 #@Time  : 2019-06-09
 #@Email : agamgn@163.com
+import os
+
 import peewee_async
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 settings={
     "static_path":"",
     "static_url_prefix":"/static/",
     "template_path":"templates",
     "secret_key":"AxfO2V3I9NetpO",
+    "jwt_expire": 7 * 24 * 3600,
+    "MEDIA_ROOT": os.path.join(BASE_DIR, "media"),
     "db":{
         "host":"127.0.0.1",
         "user":"root",
